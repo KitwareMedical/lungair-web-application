d8503a117583bea75d76d7959724e7ff2d0e9270
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2023-08-31 20:56:29 -0400

    add blood oxygen, pip, peep, and respiratory rate

--- lungair/fhir-sandbox-config/lungair_data_source.py
+++ lungair/fhir-sandbox-config/lungair_data_source.py
@@ -15,10 +15,10 @@ class LungairPatient(Patient):
 
   def get_identifier_value(self):
     return str(self.patient_info.loc['ID'])
-  
+
   def get_identifier_system(self) -> str:
     return "ID column from original data excel spreadsheet"
-  
+
   def get_dob(self) -> str:
     return self.dob.strftime("%Y-%m-%d")
 
@@ -28,7 +28,7 @@ class LungairObservation(Observation):
     """ Create an observation based on an excel spreadsheet from the LungAIR research collaboration.
 
     Args:
-      row_number: The row number from the spreadsheet that this observation came from 
+      row_number: The row number from the spreadsheet that this observation came from
       date: The date to associate to this observation. This might have to be faked to some extent.
       observation_type: One of the types available as key values in observation_types.json
       observation_value: The value taken on by the observation
@@ -73,24 +73,31 @@ class LungairDataSource(PatientDataSource):
     patient_id = patient.get_identifier_value()
     observations_for_patient_df = self.df[self.df['ID']==patient_id]
     observations = []
-    for i, row in observations_for_patient_df.iterrows():
+    for row_number, row in observations_for_patient_df.iterrows():
       date = patient.dob + datetime.timedelta(days=int(row['DOL']))
 
-      def is_missing(entry : str) -> bool:
-        """ Return whether an entry is considered to be missing data. In our tables we often have '*' indicate missing data. """
-        entry = entry.strip()
-        return entry in ['*', '']
+      for observation_type, column_name in [
+        ('FIO2', 'Supplemental O2 (FiO2)'), # fraction inspired oxygen
+        ('HR', 'HR (bpm)'), # heart rate
+        ('PIP', 'PIP (CmH2O)'), # positive inspiratory pressure
+        ('PEEP', 'PEEP (CmH2O)'), # positive end expiratory pressure
+        ('SAO2', 'SPO2 (%)'), # blood oxygen saturation
+        ('RR', 'RR (bpm)'), # respiratory rate
+      ]:
+        value_raw = str(row[column_name]).strip()
+        value_is_missing = value_raw in ['*', '']
+        if value_is_missing: continue
+        value = float(value_raw)
 
-      # fraction inspired oxygen
-      fio2_raw = str(row['Supplemental O2 (FiO2)'])
-      if not is_missing(fio2_raw):
-        fio2 = int(100*float(fio2_raw))
-        observations.append(LungairObservation(i,date,'FIO2',fio2))
-      
-      # heart rate 
-      hr_raw = str(row['HR (bpm)'])
-      if not is_missing(hr_raw):
-        hr = float(hr_raw)
-        observations.append(LungairObservation(i,date,'HR',hr))
+        if column_name=='Supplemental O2 (FiO2)': value *= 100 # the FiO2 values in our table are fractions out of 1
+
+        observations.append(
+          LungairObservation(
+            row_number = row_number,
+            date = date,
+            observation_type = observation_type,
+            observation_value = value,
+          )
+        )
 
     return observations
