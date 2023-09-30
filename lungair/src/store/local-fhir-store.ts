e4914513dad70e49c4f26d621e1553bdd329bfb5
Shreeraj Jadhav <shreerajjadhav@gmail.com>
2023-09-29 20:53:25 -0400

    feat: link EHR and DICOMWeb using PatientId
    Link the EHR records with DICOMWeb fetched images using PatientId.
    EHR will be the driving database. When user selects a patient
    from the EHR list of patients, the DICOMWeb server will be queried
    to fetch all studies associated with that patient.
    DICOMWeb is not queried until a patient is selected.

--- lungair/src/store/local-fhir-store.ts
+++ lungair/src/store/local-fhir-store.ts
@@ -1,9 +1,9 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { Ref, ref } from 'vue';
 import { useLocalStorage } from '@vueuse/core';
 
 export const useLocalFHIRStore = defineStore('local-fhir-store', () => {
-  const { VITE_LOCAL_FHIR_SERVER_NAME, VITE_LOCAL_FHIR_SERVER_URL } = import.meta.env;
+  const { VITE_LOCAL_FHIR_SERVER_NAME, VITE_LOCAL_FHIR_SERVER_URL, VITE_PATIENT_IDENTIFIER  } = import.meta.env;
 
   // GUI display name
   const hostName = VITE_LOCAL_FHIR_SERVER_NAME
@@ -15,8 +15,24 @@ export const useLocalFHIRStore = defineStore('local-fhir-store', () => {
     ? ref(VITE_LOCAL_FHIR_SERVER_URL)
     : useLocalStorage<string | null>('localFHIRServerHostURL', ''); // null if cleared by vuetify text input
 
+  const identifierSystem = ref(VITE_PATIENT_IDENTIFIER);
+
+  const currentPatientId = ref<string>('');
+
+  function setCurrentPatient(id: string) {
+    currentPatientId.value = id;
+    console.log('currentPatient is: ', currentPatientId.value);
+  }
+
+  function getCurrentPatient(): Ref<string> {
+    return currentPatientId;
+  }
+
   return {
     hostURL,
     hostName,
+    identifierSystem,
+    setCurrentPatient,
+    getCurrentPatient,
   };
 });
