e83e6b3224e72f441ccf2635ba9a62d9c881dae0
Shreeraj Jadhav <shreerajjadhav@gmail.com>
2023-10-24 13:15:47 -0400

    migrate: override App.vue

---
+++ lungair/src/components/App.vue
@@ -0,0 +1,605 @@
+<template>
+  <drag-and-drop enabled @drop-files="openFiles" id="app-container">
+    <template v-slot="{ dragHover }">
+      <v-app>
+        <v-app-bar app clipped-left :height="48">
+          <v-btn
+            v-if="mobile"
+            icon="mdi-menu"
+            @click="leftSideBar = !leftSideBar"
+          />
+          <v-toolbar-title class="d-flex flex-row align-center mt-3">
+            <vol-view-logo v-if="mobile" />
+            <vol-view-full-logo v-else />
+          </v-toolbar-title>
+          <v-btn
+            variant="text"
+            icon
+            :rounded="0"
+            class="toolbar-button"
+            href="https://volview.kitware.com/feedback/"
+            target="_blank"
+          >
+            <v-icon icon="mdi-comment-question-outline"></v-icon>
+            <v-tooltip activator="parent" location="bottom">
+              Ask Question/Submit Feedback
+            </v-tooltip>
+          </v-btn>
+          <v-btn
+            variant="text"
+            icon
+            :rounded="0"
+            class="toolbar-button"
+            @click="aboutBoxDialog = !aboutBoxDialog"
+          >
+            <v-icon icon="mdi-information-outline"></v-icon>
+            <v-tooltip activator="parent" location="bottom">About</v-tooltip>
+          </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer
+          v-model="leftSideBar"
+          app
+          clipped
+          touchless
+          width="450"
+          id="left-nav"
+        >
+          <module-panel @close="leftSideBar = false" />
+        </v-navigation-drawer>
+        <v-main id="content-main">
+          <div class="fill-height d-flex flex-row flex-grow-1">
+            <div
+              id="tools-strip"
+              class="bg-grey-darken-4 d-flex flex-column align-center"
+            >
+              <tool-button
+                size="40"
+                icon="mdi-folder-open"
+                name="Open files"
+                @click="userPromptFiles"
+              />
+              <tool-button
+                size="40"
+                icon="mdi-content-save-all"
+                name="Save session"
+                :loading="saveHappening"
+                @click="handleSave"
+              />
+              <div class="my-1 tool-separator" />
+              <v-menu location="right" :close-on-content-click="false">
+                <template v-slot:activator="{ props }">
+                  <div>
+                    <tool-button
+                      v-bind="props"
+                      size="40"
+                      icon="mdi-view-dashboard"
+                      name="Layouts"
+                    />
+                  </div>
+                </template>
+                <v-card>
+                  <v-card-text>
+                    <v-radio-group
+                      v-model="layoutName"
+                      class="mt-0"
+                      hide-details
+                    >
+                      <v-radio
+                        v-for="(value, key) in Layouts"
+                        :key="key"
+                        :label="value.name"
+                        :value="key"
+                      />
+                    </v-radio-group>
+                  </v-card-text>
+                </v-card>
+              </v-menu>
+              <template v-if="hasData">
+                <tool-strip />
+              </template>
+              <v-spacer />
+              <tool-button
+                v-if="serverUrl"
+                size="40"
+                :icon="serverConnectionIcon"
+                name="Open Server Settings"
+                @click="settingsDialog = true"
+              />
+              <v-badge
+                offset-x="10"
+                offset-y="10"
+                :content="messageCount"
+                :color="messageBadgeColor"
+                :model-value="messageCount > 0"
+              >
+                <tool-button
+                  size="40"
+                  icon="mdi-bell-outline"
+                  name="Notifications"
+                  @click="messageDialog = true"
+                />
+              </v-badge>
+              <tool-button
+                size="40"
+                icon="mdi-cog"
+                name="Settings"
+                @click="settingsDialog = true"
+              />
+            </div>
+            <div class="d-flex flex-column flex-grow-1">
+              <layout-grid v-show="hasData" :layout="currentLayout" />
+              <v-row
+                v-show="!hasData"
+                no-gutters
+                align="center"
+                class="clickable bg-grey-darken-3"
+                @click="userPromptFiles"
+              >
+                <v-col>
+                  <v-row justify="center">
+                    <v-card
+                      flat
+                      dark
+                      color="transparent"
+                      class="text-center headline"
+                    >
+                      <div>
+                        <v-icon size="64">mdi-folder-open</v-icon>
+                      </div>
+                      <div>Click to open local files.</div>
+                      <div class="mt-8">
+                        <v-icon size="64">mdi-arrow-down-bold</v-icon>
+                      </div>
+                      <div>Drag &amp; drop your DICOM files.</div>
+
+                      <div v-if="!saveUrl" class="vertical-offset-margin">
+                        <v-icon size="64">mdi-cloud-off-outline</v-icon>
+                      </div>
+                      <div v-if="!saveUrl">
+                        Secure: Image data never leaves your machine.
+                      </div>
+                      <v-btn
+                        class="mt-2"
+                        variant="tonal"
+                        color="secondary"
+                        @click.stop="dataSecurityDialog = true"
+                      >
+                        Learn More
+                      </v-btn>
+                    </v-card>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+        </v-main>
+
+        <closeable-dialog v-model="aboutBoxDialog">
+          <about-box />
+        </closeable-dialog>
+
+        <closeable-dialog v-model="messageDialog" content-class="fill-height">
+          <message-center />
+        </closeable-dialog>
+
+        <message-notifications @open-notifications="messageDialog = true" />
+
+        <closeable-dialog v-model="settingsDialog">
+          <settings />
+        </closeable-dialog>
+
+        <closeable-dialog v-model="saveDialog" max-width="30%">
+          <template v-slot="{ close }">
+            <save-session :close="close" />
+          </template>
+        </closeable-dialog>
+
+        <closeable-dialog v-model="dataSecurityDialog">
+          <data-security-box />
+        </closeable-dialog>
+
+        <keyboard-shortcuts />
+      </v-app>
+      <persistent-overlay
+        :disabled="!dragHover"
+        color="#000"
+        :opacity="0.3"
+        :z-index="2000"
+        class="text-center"
+      >
+        <div class="d-flex flex-column align-center justify-center h-100">
+          <div class="dnd-prompt">
+            <v-icon size="4.75rem">mdi-download</v-icon>
+            <div class="text-h2 font-weight-bold">Drop your files to open</div>
+          </div>
+        </div>
+      </persistent-overlay>
+    </template>
+  </drag-and-drop>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  nextTick,
+  onMounted,
+  Ref,
+  ref,
+  watch,
+} from 'vue';
+import { storeToRefs } from 'pinia';
+import { UrlParams } from '@vueuse/core';
+import vtkURLExtract from '@kitware/vtk.js/Common/Core/URLExtract';
+import { URL } from 'whatwg-url';
+import { useDisplay } from 'vuetify';
+
+import { basename } from '@/src/utils/path';
+import { useDatasetStore } from '@/src/store/datasets';
+import { logError } from '@/src/utils/loggers';
+import {
+  importDataSources,
+  ImportDataSourcesResult,
+  convertSuccessResultToDataSelection,
+} from '@/src/io/import/importDataSources';
+import ToolButton from './ToolButton.vue';
+import LayoutGrid from './LayoutGrid.vue';
+import ModulePanel from './ModulePanel.vue';
+import DragAndDrop from './DragAndDrop.vue';
+import CloseableDialog from './CloseableDialog.vue';
+import AboutBox from './AboutBox.vue';
+import ToolStrip from './ToolStrip.vue';
+import MessageCenter from './MessageCenter.vue';
+import MessageNotifications from './MessageNotifications.vue';
+import Settings from './Settings.vue';
+import PersistentOverlay from './PersistentOverlay.vue';
+import DataSecurityBox from './DataSecurityBox.vue';
+import VolViewFullLogo from './icons/VolViewFullLogo.vue';
+import VolViewLogo from './icons/VolViewLogo.vue';
+import KeyboardShortcuts from './KeyboardShortcuts.vue';
+import {
+  DataSource,
+  fileToDataSource,
+  getDataSourceName,
+  uriToDataSource,
+} from '../io/import/dataSource';
+import { useImageStore } from '../store/datasets-images';
+import { useViewStore } from '../store/views';
+import { ConnectionState, useServerStore } from '../store/server';
+import { MessageType, useMessageStore } from '../store/messages';
+import { Layouts } from '../config';
+import { serialize } from '../io/state-file';
+import SaveSession from './SaveSession.vue';
+import { useGlobalErrorHook } from '../composables/useGlobalErrorHook';
+import { useWebGLWatchdog } from '../composables/useWebGLWatchdog';
+import { useAppLoadingNotifications } from '../composables/useAppLoadingNotifications';
+import { partition, wrapInArray } from '../utils';
+import { useKeyboardShortcuts } from '../composables/useKeyboardShortcuts';
+
+async function loadFiles(
+  sources: DataSource[],
+  setError: (err: Error) => void
+) {
+  const dataStore = useDatasetStore();
+
+  let results: ImportDataSourcesResult[];
+  try {
+    results = await importDataSources(sources);
+  } catch (error) {
+    setError(error as Error);
+    return;
+  }
+
+  const [succeeded, errored] = partition((result) => result.ok, results);
+
+  if (!dataStore.primarySelection && succeeded.length) {
+    const selection = convertSuccessResultToDataSelection(succeeded[0]);
+    if (selection) {
+      dataStore.setPrimarySelection(selection);
+    }
+  }
+
+  if (errored.length) {
+    const errorMessages = errored.map((errResult) => {
+      // pick first error
+      const [firstError] = errResult.errors;
+      // pick innermost dataset that errored
+      const name = getDataSourceName(firstError.inputDataStackTrace[0]);
+      // log error for debugging
+      logError(firstError.cause);
+      return `- ${name}: ${firstError.message}`;
+    });
+    const failedError = new Error(
+      `These files failed to load:\n${errorMessages.join('\n')}`
+    );
+
+    setError(failedError);
+  }
+}
+
+async function loadRemoteFilesFromURLParams(
+  params: UrlParams,
+  setError: (err: Error) => void
+) {
+  const urls = wrapInArray(params.urls);
+  const names = wrapInArray(params.names ?? []); // optional names should resolve to [] if params.names === undefined
+  const sources = urls.map((url, idx) =>
+    uriToDataSource(
+      url,
+      names[idx] || basename(new URL(url, window.location.href).pathname) || url
+    )
+  );
+
+  await loadFiles(sources, setError);
+}
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    ToolButton,
+    LayoutGrid,
+    DragAndDrop,
+    CloseableDialog,
+    AboutBox,
+    DataSecurityBox,
+    ToolStrip,
+    ModulePanel,
+    MessageCenter,
+    MessageNotifications,
+    VolViewFullLogo,
+    VolViewLogo,
+    Settings,
+    SaveSession,
+    PersistentOverlay,
+    KeyboardShortcuts,
+  },
+
+  setup() {
+    const imageStore = useImageStore();
+    const messageStore = useMessageStore();
+    const viewStore = useViewStore();
+
+    useGlobalErrorHook();
+    useWebGLWatchdog();
+    useKeyboardShortcuts();
+
+    const { runAsLoading } = useAppLoadingNotifications();
+
+    // --- layout --- //
+
+    const layoutName: Ref<string> = ref('Quad View');
+    const { layout: currentLayout } = storeToRefs(viewStore);
+
+    watch(
+      layoutName,
+      () => {
+        const layout = Layouts[layoutName.value] || [];
+        viewStore.setLayout(layout);
+      },
+      {
+        immediate: true,
+      }
+    );
+
+    watch(currentLayout, () => {
+      if (
+        currentLayout.value?.name &&
+        currentLayout.value.name !== layoutName.value
+      ) {
+        layoutName.value = currentLayout.value.name;
+      }
+    });
+
+    // --- file handling --- //
+
+    async function openFiles(files: FileList | null) {
+      if (!files) {
+        return;
+      }
+
+      const dataSources = Array.from(files).map(fileToDataSource);
+      runAsLoading((setError) => loadFiles(dataSources, setError));
+    }
+
+    const fileEl = document.createElement('input');
+    fileEl.setAttribute('type', 'file');
+    fileEl.setAttribute('multiple', 'multiple');
+    fileEl.setAttribute('accept', '*');
+    fileEl.addEventListener('change', () => openFiles(fileEl.files));
+
+    function userPromptFiles() {
+      fileEl.value = '';
+      fileEl.click();
+    }
+
+    // --- parse URL -- //
+
+    const urlParams = vtkURLExtract.extractURLParameters() as UrlParams;
+
+    onMounted(() => {
+      if (!urlParams.urls) {
+        return;
+      }
+
+      // TODO remove this nextTick when we switch away from
+      // vue-toastification.
+      // We run in nextTick to ensure the library is mounted.
+      nextTick(() => {
+        runAsLoading((setError) =>
+          loadRemoteFilesFromURLParams(urlParams, setError)
+        );
+      });
+    });
+
+    // --- remote server --- //
+
+    const serverStore = useServerStore();
+    const { url: serverUrl } = storeToRefs(serverStore);
+
+    const serverConnectionIcon = computed(() => {
+      switch (serverStore.connState) {
+        case ConnectionState.Connected:
+          return 'mdi-lan-check';
+        case ConnectionState.Disconnected:
+          return 'mdi-lan-disconnect';
+        case ConnectionState.Pending:
+          return 'mdi-lan-pending';
+        default:
+          throw new Error('Invalid connection state');
+      }
+    });
+
+    onMounted(() => {
+      serverStore.connect();
+    });
+
+    // --- --- //
+
+    const hasData = computed(() => imageStore.idList.length > 0);
+    const messageCount = computed(() => messageStore.importantMessages.length);
+    const messageBadgeColor = computed(() => {
+      if (
+        messageStore.importantMessages.find(
+          (msg) => msg.type === MessageType.Error
+        )
+      ) {
+        return 'error';
+      }
+      if (
+        messageStore.importantMessages.find(
+          (msg) => msg.type === MessageType.Warning
+        )
+      ) {
+        return 'warning';
+      }
+      return 'primary';
+    });
+
+    const saveDialog = ref(false);
+    const saveUrl =
+      import.meta.env.VITE_ENABLE_REMOTE_SAVE && (urlParams.save as string);
+    const saveHappening = ref(false);
+
+    const handleSave = async () => {
+      if (saveUrl) {
+        try {
+          saveHappening.value = true;
+
+          const blob = await serialize();
+          const saveResult = await fetch(saveUrl, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/zip',
+              'Content-Length': blob.size.toString(),
+            },
+            body: blob,
+          });
+
+          if (saveResult.ok) messageStore.addSuccess('Save Successful');
+          else messageStore.addError('Save Failed', 'Network response not OK');
+        } catch (error) {
+          messageStore.addError(
+            'Save Failed with error',
+            `Failed from: ${error}`
+          );
+        } finally {
+          saveHappening.value = false;
+        }
+      } else {
+        saveDialog.value = true;
+      }
+    };
+
+    const display = useDisplay();
+
+    return {
+      aboutBoxDialog: ref(false),
+      messageDialog: ref(false),
+      settingsDialog: ref(false),
+      dataSecurityDialog: ref(false),
+      saveDialog,
+      handleSave,
+      saveHappening,
+      leftSideBar: ref(!display.mobile.value),
+      mobile: display.mobile,
+      messageCount,
+      messageBadgeColor,
+      layoutName,
+      currentLayout,
+      Layouts,
+      userPromptFiles,
+      openFiles,
+      hasData,
+      saveUrl,
+      serverConnectionIcon,
+      serverUrl,
+    };
+  },
+});
+</script>
+
+<style>
+#content-main {
+  /* disable v-content transition when we resize our app drawer */
+  transition: initial;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+}
+
+#left-nav {
+  border-right: 1px solid rgb(var(--v-theme-background));
+}
+
+#content-main > .v-content__wrap {
+  display: flex;
+}
+
+#module-switcher .v-input__prepend-inner {
+  /* better icon alignment */
+  margin-top: 15px;
+}
+
+.alert > .v-snack__wrapper {
+  /* transition background color */
+  transition: background-color 0.25s;
+}
+</style>
+
+<style src="@/src/components/styles/utils.css"></style>
+
+<style scoped>
+#app-container {
+  width: 100%;
+  height: 100%;
+}
+
+#tools-strip {
+  border-left: 1px solid #212121;
+  flex: 0 0 40px;
+}
+
+.toolbar-button {
+  min-height: 100%; /* fill toolbar height */
+}
+
+.tool-separator {
+  width: 75%;
+  height: 1px;
+  border: none;
+  border-top: 1px solid rgb(112, 112, 112);
+}
+
+.vertical-offset-margin {
+  margin-top: 128px;
+}
+
+.dnd-prompt {
+  background: rgba(0, 0, 0, 0.4);
+  color: white;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.4);
+  padding: 64px;
+}
+</style>
--- lungair/src/config.ts
+++ lungair/src/config.ts
@@ -1,4 +1,230 @@
-import { Layouts } from '@/src/config';
-export * from '@/src/config';
+import MRICardiacThumbnail from '@/src/assets/samples/MRI-Cardiac.jpg';
+import MRIPROSTATExThumbnail from '@/src/assets/samples/MRI-PROSTATEx.jpg';
+import MRAHeadThumbnail from '@/src/assets/samples/MRA-Head_and_Neck.jpg';
+import CTAHeadThumbnail from '@/src/assets/samples/CTA-Head_and_Neck.jpg';
+import USFetusThumbnail from '@/src/assets/samples/3DUS-Fetus.jpg';
+import { Layout, LayoutDirection } from './types/layout';
+import { ViewSpec } from './types/views';
+import { SampleDataset } from './types';
+import { Action } from './constants';
 
-Layouts['Axial Only'].name = 'Custom Axial';
+/**
+ * These are the initial view IDs.
+ */
+export const InitViewIDs: Record<string, string> = {
+  Coronal: 'Coronal',
+  Sagittal: 'Sagittal',
+  Axial: 'Axial',
+  Three: '3D',
+};
+
+/**
+ * View spec for the initial view IDs.
+ */
+export const InitViewSpecs: Record<string, ViewSpec> = {
+  [InitViewIDs.Coronal]: {
+    viewType: '2D',
+    props: {
+      viewDirection: 'Right',
+      viewUp: 'Superior',
+    },
+  },
+  [InitViewIDs.Sagittal]: {
+    viewType: '2D',
+    props: {
+      viewDirection: 'Posterior',
+      viewUp: 'Superior',
+    },
+  },
+  [InitViewIDs.Axial]: {
+    viewType: '2D',
+    props: {
+      viewDirection: 'Superior',
+      viewUp: 'Anterior',
+    },
+  },
+  [InitViewIDs.Three]: {
+    viewType: '3D',
+    props: {
+      viewDirection: 'Posterior',
+      viewUp: 'Superior',
+    },
+  },
+};
+
+/**
+ * The default view spec.
+ */
+export const DefaultViewSpec = InitViewSpecs[InitViewIDs.Axial];
+
+/**
+ * Defines the default layouts.
+ */
+export const Layouts: Record<string, Layout> = [
+  {
+    name: 'Axial Only',
+    direction: LayoutDirection.H,
+    items: [InitViewIDs.Axial],
+  },
+  {
+    name: 'Axial Primary',
+    direction: LayoutDirection.V,
+    items: [
+      InitViewIDs.Axial,
+      {
+        direction: LayoutDirection.H,
+        items: [InitViewIDs.Three, InitViewIDs.Coronal, InitViewIDs.Sagittal],
+      },
+    ],
+  },
+  {
+    name: '3D Primary',
+    direction: LayoutDirection.V,
+    items: [
+      InitViewIDs.Three,
+      {
+        direction: LayoutDirection.H,
+        items: [InitViewIDs.Coronal, InitViewIDs.Sagittal, InitViewIDs.Axial],
+      },
+    ],
+  },
+  {
+    name: 'Quad View',
+    direction: LayoutDirection.H,
+    items: [
+      {
+        direction: LayoutDirection.V,
+        items: [InitViewIDs.Coronal, InitViewIDs.Three],
+      },
+      {
+        direction: LayoutDirection.V,
+        items: [InitViewIDs.Sagittal, InitViewIDs.Axial],
+      },
+    ],
+  },
+  {
+    name: '3D Only',
+    direction: LayoutDirection.H,
+    items: [InitViewIDs.Three],
+  },
+].reduce((layouts, layout) => {
+  return { ...layouts, [layout.name]: layout };
+}, {});
+
+export const LABELMAP_PALETTE = {
+  0: [0, 0, 0, 0], // eraser
+  1: [153, 153, 0, 255],
+  2: [76, 76, 0, 255],
+  3: [255, 255, 0, 255],
+  4: [0, 76, 0, 255],
+  5: [0, 153, 0, 255],
+  6: [0, 255, 0, 255],
+  7: [76, 0, 0, 255],
+  8: [153, 0, 0, 255],
+  9: [255, 0, 0, 255],
+  10: [0, 76, 76, 255],
+  11: [0, 153, 153, 255],
+  12: [0, 255, 255, 255],
+  13: [0, 0, 76, 255],
+  14: [0, 0, 153, 255],
+} as Record<number, number[]>;
+
+export const SAMPLE_DATA: SampleDataset[] = [
+  {
+    name: 'CTA Head and Neck',
+    filename: 'CTA-Head_and_Neck.zip',
+    description:
+      'CTA head and neck scan of elderly patient with tumor. (80 MB)',
+    url: 'https://data.kitware.com/api/v1/item/6347159711dab81428208e24/download',
+    image: CTAHeadThumbnail,
+  },
+  {
+    name: 'MRA Head and Neck',
+    filename: 'MRA-Head_and_Neck.zip',
+    description:
+      'MRA from Patient Contributed Image Repository. Click application help icon "(?)" for more info. (15 MB)',
+    url: 'https://data.kitware.com/api/v1/item/6352a2b311dab8142820a33b/download',
+    image: MRAHeadThumbnail,
+  },
+  {
+    name: 'MRI Cardiac 3D and Cine',
+    filename: 'MRI-Cardiac-3D_and_Cine.zip',
+    description:
+      'MRI scan with two series: 3D axial non-gated and 2 chamber cine. (4 MB)',
+    url: 'https://data.kitware.com/api/v1/item/6350b28f11dab8142820949d/download',
+    image: MRICardiacThumbnail,
+  },
+  {
+    name: 'MRI PROSTATEx',
+    filename: 'MRI-PROSTATEx-0004.zip',
+    description:
+      'MRI from the SPIE-AAPM-NCI PROSTATEx challenge. Click application help "(?)" icon for more info. (3 MB)',
+    url: 'https://data.kitware.com/api/v1/item/63527c7311dab8142820a338/download',
+    image: MRIPROSTATExThumbnail,
+  },
+  {
+    name: '3D US Fetus',
+    filename: '3DUS-Fetus.mha',
+    description:
+      '3D ultrasound of a baby. Downloaded from tomovision.com.(8 MB)',
+    url: 'https://data.kitware.com/api/v1/item/635679c311dab8142820a4f4/download',
+    image: USFetusThumbnail,
+    defaults: {
+      colorPreset: 'US-Fetal',
+    },
+  },
+];
+
+export const TOOL_COLORS = [
+  '#58f24c',
+  '#8de4d3',
+  '#f0a4b1',
+  '#a3c9fe',
+  '#c8f251',
+  '#fea53b',
+];
+
+export const STROKE_WIDTH_ANNOTATION_TOOL_DEFAULT = 1;
+
+export const RULER_LABEL_DEFAULTS = {
+  red: { color: 'red' },
+  green: { color: '#00ff00' },
+  white: { color: '#ffffff' },
+};
+
+export const RECTANGLE_LABEL_DEFAULTS = {
+  artifact: { color: '#888888', fillColor: '#ffbf0040' },
+  innocuous: { color: '#00ff00', fillColor: '#00ff0020' },
+  lesion: { color: 'red', fillColor: 'transparent' },
+};
+
+export const POLYGON_LABEL_DEFAULTS = {
+  red: { color: 'red' },
+  green: { color: '#00ff00' },
+  white: { color: '#ffffff' },
+};
+
+export const DEFAULT_PRESET_BY_MODALITY: Record<string, string> = {
+  CT: 'CT-AAA',
+  MR: 'CT-Coronary-Arteries-2',
+  US: 'US-Fetal',
+};
+
+// Keyboard shortcuts/hotkeys. Can add modifiers: 'Shift+Ctrl+A'
+export const ACTION_TO_KEY = {
+  windowLevel: 'l',
+  pan: 'n',
+  zoom: 'z',
+  ruler: 'm',
+  paint: 'p',
+  rectangle: 'r',
+  crosshairs: 'c',
+  crop: 'b',
+  polygon: 'g',
+  select: 's',
+
+  decrementLabel: 'q',
+  incrementLabel: 'w',
+
+  showKeyboardShortcuts: '?',
+} satisfies Record<Action, string>;
