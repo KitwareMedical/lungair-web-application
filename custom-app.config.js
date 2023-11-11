8b0364b475a67c6c85ee7e3fc894de984e570cda
Shreeraj Jadhav <shreerajjadhav@gmail.com>
2023-11-10 20:33:38 -0500

    fix: volview submodule issues

--- custom-app.config.js
+++ custom-app.config.js
@@ -3,6 +3,6 @@ export default {
   coreDir: "core/VolView",
   coreRepo: {
     url: "https://github.com/Kitware/VolView.git",
-    ref: "main",
+    ref: "b59bbae9523f6ac09dff092bf1f5c4db5015b7f8",
   },
 };
