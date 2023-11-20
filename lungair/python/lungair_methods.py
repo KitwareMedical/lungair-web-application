ff53bab50fe1b5ac652fe65814643e5650fe6c2c
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2023-11-19 22:40:48 -0500

    adapt lungair_methods.py for new VolView python server design
    The server/methods/ directory used to be built into core volview, and our lungair-main branch when we forked core volview relied on this directory being there.
    Now that after the migration, this directory is gone from the more recent core volview that we are pinning our submodule to.
    We can't go back to an earlier volview because the python server didn't exist when we forked.
    I copied from this newer example to updated how lungair_methods.py works:
    https://github.com/Kitware/VolView/blob/411e5a891bfb520647ab3f97cac6edfcca930a65/server/examples/example_api.py
    
    Fix lungair_methods.py to work with the newer VolView python server design

--- lungair/python/lungair_methods.py
+++ lungair/python/lungair_methods.py
@@ -1,9 +1,8 @@
 import asyncio
+from dataclasses import dataclass, field
 from concurrent.futures import ProcessPoolExecutor
 
 import itk
-
-itk.force_load()
 
 from volview_server import (
     VolViewApi,
@@ -16,70 +15,87 @@ from volview_server.transformers import (
     convert_vtkjs_to_itk_image,
 )
 
-from methods.methods_utils import (
-    show_image,
-    CommonClientState,
-)
-
-from methods.median_filter_method import (
-    MedianFilterClientState,
-    median_filter_method,
-)
-
-
 ## Link to app ##
 
 
 volview = VolViewApi()
 
 
-## Allocate process pool ##
-
+## median filter example ##
+# copied from
+# https://github.com/Kitware/VolView/blob/411e5a891bfb520647ab3f97cac6edfcca930a65/server/examples/example_api.py
 
 process_pool = ProcessPoolExecutor(4)
 
 
-## Compose the Client State ##
+@dataclass
+class ClientState:
+    image_id_map: dict = field(init=False, default_factory=dict)
+    blurred_ids: set = field(init=False, default_factory=set)
 
 
-class CurrentClientState:
-    def __init__(self):
-        self.common = CommonClientState()
-        self.median_filter = MedianFilterClientState()
+def do_median_filter(serialized_img, radius):
+    img = convert_vtkjs_to_itk_image(serialized_img)
+    ImageType = type(img)
+
+    median_filter = itk.MedianImageFilter[ImageType, ImageType].New()
+    median_filter.SetInput(img)
+    median_filter.SetRadius(radius)
+    median_filter.Update()
+
+    output = median_filter.GetOutput()
+    return convert_itk_to_vtkjs_image(output)
 
 
-## Median filter ##
+async def run_median_filter_process(img, radius: int):
+    serialized_img = convert_itk_to_vtkjs_image(img)
+    loop = asyncio.get_event_loop()
+    serialized_output = await loop.run_in_executor(
+        process_pool, do_median_filter, serialized_img, radius
+    )
+    return convert_vtkjs_to_itk_image(serialized_output)
+
+
+def associate_images(state, image_id, blurred_id):
+    state.blurred_ids.add(blurred_id)
+    state.image_id_map[image_id] = blurred_id
+    state.image_id_map[blurred_id] = image_id
+
+
+def get_base_image(state: ClientState, img_id: str) -> str:
+    if img_id in state.blurred_ids:
+        return state.image_id_map[img_id]
+    return img_id
+
+
+async def show_image(img_id: str):
+    store = get_current_client_store("dataset")
+    await store.setPrimarySelection({"type": "image", "dataID": img_id})
 
 
 @volview.expose("medianFilter")
-async def median_filter_api(input_image_id, radius):
-    print(f"Started median filter on {input_image_id}.")
+async def median_filter(img_id, radius):
+    print(f"Started median filter on {img_id} with radius {radius}...")
     store = get_current_client_store("images")
-    state = get_current_session(default_factory=CurrentClientState)
+    state = get_current_session(default_factory=ClientState)
 
-    # Set state variables used by median filter - current_image
-    image = await store.dataIndex[input_image_id]
-    serialized_image = convert_itk_to_vtkjs_image(image)
-    state.common.set_current_image(input_image_id, serialized_image)
-
-    # Set state variables used by median filter - radius
-    state.median_filter.set_radius(radius)
+    # Behavior: when a median filter request occurs on a
+    # blurred image, we instead assume we are re-running
+    # the blur operation on the original image.
+    base_image_id = get_base_image(state, img_id)
+    img = await store.dataIndex[base_image_id]
 
     # we need to run the median filter in a subprocess,
     # since itk blocks the GIL.
-    loop = asyncio.get_event_loop()
-    serialized_output = await loop.run_in_executor(
-        process_pool,
-        median_filter_method,
-        state,
-    )
+    output = await run_median_filter_process(img, radius)
+    print(f"Completed median filter on {img_id} with radius {radius}.")
 
-    # convert image to back to itkImage
-    output_image = convert_vtkjs_to_itk_image(serialized_output)
-    output_image_id = await store.addVTKImageData(
-        "Median filtered image",
-        output_image,
-    )
-    state.common.associate_derived_image(input_image_id, output_image_id)
+    blurred_id = state.image_id_map.get(base_image_id)
+    if not blurred_id:
+        blurred_id = await store.addVTKImageData("Blurred image", output)
+        # Associate the blurred image ID with the base image ID.
+        associate_images(state, base_image_id, blurred_id)
+    else:
+        await store.updateData(blurred_id, output)
 
-    await show_image(output_image_id)
+    await show_image(blurred_id)
